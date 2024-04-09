9f3de3756aa235d98c48d85434f346cc7f6ab443
vaseemshaykh <shaykhvaseem69@gmail.com>
2024-04-10 00:58:14 +0530

    refactored package commit

---
+++ src/main/java/com/myproject/cabbooking/exception/CustomException.java
@@ -0,0 +1,9 @@
+package com.myproject.cabbooking.exception;
+
+@SuppressWarnings("serial")
+public class CustomException extends Exception {
+	public CustomException(String message) {
+		super(message);
+	}
+
+}
